1d54db6b3408391d89bfe43ad74358fc29dd148c
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-10-23 21:00:48 -0700

    Update Color

--- GitStatus/Pages/StatusPage.cs
+++ GitStatus/Pages/StatusPage.cs
@@ -7,17 +7,19 @@ namespace GitStatus
     {
         public StatusPage(StatusViewModel statusViewModel) : base(statusViewModel, "GitHub Api Status")
         {
+            BackgroundColor = Color.White;
+
             Content = new StackLayout
             {
                 Children =
                 {
-                    new Label().Center().TextCenter()
+                    new Label { TextColor = Color.Black }.Center().TextCenter()
                         .Bind(Label.TextProperty, nameof(StatusViewModel.StatusLabelText)),
 
                     new Button { Text = "Get Status"}.Center()
                         .Bind(Button.CommandProperty, nameof(StatusViewModel.GetStatusCommand)),
 
-                    new ActivityIndicator().Center()
+                    new ActivityIndicator { Color = Color.Black }.Center()
                         .Bind(IsVisibleProperty, nameof(StatusViewModel.IsBusy))
                         .Bind(ActivityIndicator.IsRunningProperty, nameof(StatusViewModel.IsBusy))
                 }
