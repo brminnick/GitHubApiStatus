7c062ce4c7236e42131cb5568f248970147830cf
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-10-25 15:26:43 -0700

    Update GitHubConstants.cs

--- GitStatus.Mobile/Constants/GitHubConstants.cs
+++ GitStatus.Mobile/Constants/GitHubConstants.cs
@@ -3,8 +3,8 @@ namespace GitStatus.Mobile
 {
     static class GitHubConstants
     {
-//#error Missing Token, Follow these steps to create your Personal Access Token: https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/#creating-a-token
-        public const string PersonalAccessToken = "4c9758a124ed71474986d8d34d0ef169975f8eb7";
+#error Missing Token, Follow these steps to create your Personal Access Token: https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/#creating-a-token
+        public const string PersonalAccessToken = "";
         public const string GitHubRestApiUrl = "https://api.github.com";
         public const string GitHubGraphQLApiUrl = "https://api.github.com/graphql";
     }
