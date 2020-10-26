c61bfad1aa18b66d0ce65168676e2f3ee5edf761
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-10-25 18:23:00 -0700

    Add Src Folder

---
+++ Src/GitStatus/Constants/GitHubConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace GitStatus
+{
+    static class GitHubConstants
+    {
+//#error Missing Token, Follow these steps to create your Personal Access Token: https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/#creating-a-token
+        public const string PersonalAccessToken = "";
+        public const string GitHubRestApiUrl = "https://api.github.com";
+        public const string GitHubGraphQLApiUrl = "https://api.github.com/graphql";
+    }
+}
