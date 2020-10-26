e682db7e84c1d7577e33e742f65c799db4aa47b4
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-10-25 18:24:35 -0700

    Re-enable Compiler Error

--- Src/GitStatus/Constants/GitHubConstants.cs
+++ Src/GitStatus/Constants/GitHubConstants.cs
@@ -2,7 +2,7 @@
 {
     static class GitHubConstants
     {
-//#error Missing Token, Follow these steps to create your Personal Access Token: https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/#creating-a-token
+#error Missing Token, Follow these steps to create your Personal Access Token: https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/#creating-a-token
         public const string PersonalAccessToken = "";
         public const string GitHubRestApiUrl = "https://api.github.com";
         public const string GitHubGraphQLApiUrl = "https://api.github.com/graphql";
