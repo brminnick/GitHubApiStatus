9ff5f41a6909121472383ce16d38608fa555934b
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-10-26 17:21:42 -0700

    Add GitStatus.Console

--- Src/GitStatus.Shared/Constants/GitHubConstants.cs
+++ Src/GitStatus.Shared/Constants/GitHubConstants.cs
@@ -1,8 +1,7 @@
-﻿namespace GitStatus
+﻿namespace GitStatus.Shared
 {
     static class GitHubConstants
     {
-#error Missing Token, Follow these steps to create your Personal Access Token: https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/#creating-a-token
         public const string PersonalAccessToken = "";
         public const string GitHubRestApiUrl = "https://api.github.com";
         public const string GitHubGraphQLApiUrl = "https://api.github.com/graphql";
--- Src/GitStatus/Services/GitHubStatusService.cs
+++ Src/GitStatus/Services/GitHubStatusService.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using GitStatus.Shared;
 using GraphQL;
 using Polly;
 using Refit;
