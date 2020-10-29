df0098425838d3cb67b0d15c92af997df77d2bec
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-10-28 18:57:47 -0700

    Remove GraphQL.Client Dependency

---
+++ Src/GitHubApiStatus.UnitTests/Models/GraphQLRequest.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace GitHubApiStatus.UnitTests
+{
+    class GraphQLRequest
+    {
+        public GraphQLRequest(string query, string variables = "")
+        {
+            Query = query;
+            Variables = variables;
+        }
+
+        [JsonProperty("query")]
+        public string Query { get; }
+
+        [JsonProperty("variables")]
+        public string Variables { get; }
+    }
+}
