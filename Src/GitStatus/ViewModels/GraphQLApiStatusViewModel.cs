6503c66487d116b611ba7fea1830697e3b3bae87
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-11-27 17:08:28 -0800

    Update Blazor Web App

--- Src/GitStatus/ViewModels/GraphQLApiStatusViewModel.cs
+++ Src/GitStatus/ViewModels/GraphQLApiStatusViewModel.cs
@@ -16,10 +16,7 @@ namespace GitStatus
             var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(5));
             var apiRateLimitStatuses = await _gitHubApiStatusService.GetApiRateLimits(cancellationTokenSource.Token).ConfigureAwait(false);
 
-            StatusLabelText = @$"Rate Limit: {apiRateLimitStatuses.GraphQLApi.RateLimit}
-Remaining Request Count: {apiRateLimitStatuses.GraphQLApi.RemainingRequestCount}
-Rate Limit Reset: {apiRateLimitStatuses.GraphQLApi.RateLimitReset_DateTime:dd MMMM @ HH:mm}
-Reset Time Remainaing: {apiRateLimitStatuses.GraphQLApi.RateLimitReset_TimeRemaining}";
+            StatusLabelText = apiRateLimitStatuses.GraphQLApi.ToString();
         }
     }
 }
