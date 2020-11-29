37722155b717f9fb41051c2a97f5125fcd310328
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-11-29 10:19:30 -0800

    Add GitStatus.API

---
+++ Src/GitStatus.Web/GitStatus.API/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http.Headers;
+using GitHubApiStatus.Extensions;
+using GitStatus.API;
+using GitStatus.Shared;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace GitStatus.API
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddGitHubApiStatusService(new AuthenticationHeaderValue(GitHubConstants.AuthScheme, GitHubConstants.PersonalAccessToken), new ProductHeaderValue(nameof(GitStatus)));
+        }
+    }
+}
