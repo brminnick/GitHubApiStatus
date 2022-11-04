5800a412999004731a0a56a2bb91ea20835caabe
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-11-04 13:52:42 -0700

    Update GitHubConstants.cs

--- samples/GitStatus.Shared/Constants/GitHubConstants.cs
+++ samples/GitStatus.Shared/Constants/GitHubConstants.cs
@@ -3,7 +3,7 @@
     static class GitHubConstants
     {
         public const string AuthScheme = "bearer";
-        public const string PersonalAccessToken = "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";
+        public const string PersonalAccessToken = "";
         public const string GitHubRestApiUrl = "https://api.github.com";
         public const string GitHubGraphQLApiUrl = "https://api.github.com/graphql";
     }
