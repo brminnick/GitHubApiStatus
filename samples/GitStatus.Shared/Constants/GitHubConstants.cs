ee12ce39a959f068167fd6e8539fdb7ec3ca6ffd
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-11-04 13:34:50 -0700

    Update Folder Structure

---
+++ samples/GitStatus.Shared/Constants/GitHubConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace GitStatus.Shared
+{
+    static class GitHubConstants
+    {
+        public const string AuthScheme = "bearer";
+        public const string PersonalAccessToken = "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";
+        public const string GitHubRestApiUrl = "https://api.github.com";
+        public const string GitHubGraphQLApiUrl = "https://api.github.com/graphql";
+    }
+}
